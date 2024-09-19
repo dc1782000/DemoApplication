dffb90dde1ca4b32d3c5bb4d28ab203dcfde8163
Dinesh Chandra Sharma <104714574+dc1782000@users.noreply.github.com>
2024-09-20 02:12:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddHttpClient("YourApiClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:8585/");
+    // Add any other HttpClient configuration here
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = false;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Strict;
+
+        options.LoginPath = "/MyLoginSinup/Index"; // Redirect to the login page if not authenticated
+        options.AccessDeniedPath = "/Home/Index"; // Redirect to the access denied page if not authorized
+    });
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=MyLoginSignUp}/{action=Index}/{id?}");
+
+app.Run();
+
